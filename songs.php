59aa73e2ce62f6df3b8ad2db08d474862515463e
Leo Villalobos <villaleobos@gmail.com>
2024-11-21 13:13:21 -0800

    Create songs list (#2)
    * add songs list
    
    * add image
    
    ---------
    
    Co-authored-by: fernanddaniel <danielf.amaya07@gmail.com>

--- songs.php
+++ songs.php
@@ -1,9 +1,24 @@
 <?php
 $songs = [
-    0 => [
-        'cover' => 'images/Playlist_pic.png',
+    [
+        'cover' => 'images/Maybe-next-week.png',
         'title' => 'maybe next week',
-        'album' => 'Harmless',
+        'artist' => 'Harmless',
+        'album' => 'Maybe Next Week',
         'duration' => '3:25',
-    ]
+    ],
+    [
+        'cover' => 'images/Maybe-next-week.png',
+        'title' => 'maybe week',
+        'artist' => 'Harmless',
+        'album' => '',
+        'duration' => '3:25',
+    ],
+    [
+        'cover' => 'images/Maybe-next-week.png',
+        'title' => 'week',
+        'artist' => 'Harmless',
+        'album' => '',
+        'duration' => '3:25',
+    ],
 ];
